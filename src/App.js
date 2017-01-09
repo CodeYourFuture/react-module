f15cf52a5c38b3fbbd4705d3cd77dfcf08474fc3
Angela <Baby007mix@gmail.com>
2017-01-09 14:18:42 +0000

    Starting with React.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import response from './rams';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to RAMS</h2>
+        </div>
+        <p className="App-intro">
+         {JSON.stringify(response)}
+         response.website
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
